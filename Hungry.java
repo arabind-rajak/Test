9c89f832920d6acd28eb01cae45a778399dfbb36
Arabind Rajak <arabindrajak9143@gmail.com>
2023-01-10 19:27:31 +0530

    Revert "for check revert is applicable"
    This reverts commit 6aa05826a7ee70e65d396250dbdcf708478650aa.

--- Hungry.java
+++ Hungry.java
@@ -1,6 +1,5 @@
 class Hungry{
 	public static void main(String[] args){
-		System.out.println("I am very hungry i need maggi");
-		System.out.println("you can drink water");
+		System.out.println("I am very hungry i need maggi ");
 	}
 }
