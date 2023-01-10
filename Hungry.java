e9463defa8d11b36be570f1ea2f20470d8da90c6
Arabind Rajak <arabindrajak9143@gmail.com>
2023-01-10 20:03:45 +0530

    water

--- Hungry.java
+++ Hungry.java
@@ -1,5 +1,6 @@
 class Hungry{
 	public static void main(String[] args){
 		System.out.println("I am very hungry i need maggi ");
+		System.out.println("Are u thirsty if yes then drink water");
 	}
 }
