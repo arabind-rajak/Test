0b9089442c1501ef51ba9ada5b2091ce745083d4
Arabind Rajak <arabindrajak9143@gmail.com>
2023-01-10 20:29:07 +0530

    Type of drink

--- Hungry.java
+++ Hungry.java
@@ -1,6 +1,7 @@
 class Hungry{
 	public static void main(String[] args){
-		System.out.println("I am very hungry i need maggi ");
+		
 		System.out.println("Are u thirsty if yes then drink water");
+		System.out.println("You can also take cold drink, ice-cream, soda as well");
 	}
 }
